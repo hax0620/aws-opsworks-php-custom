ac82c26e00152043b4d0ca634c12be05557a201b
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-27 14:24:19 -0400

    Install and execute composer during deployment

--- webservices/recipes/appsetup.rb
+++ webservices/recipes/appsetup.rb
@@ -1,5 +1,15 @@
 node[:deploy].each do |app_name, deploy|
 
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -sS https://getcomposer.org/installer | php
+    php composer.phar install
+    EOH
+  end
+  
   template "#{deploy[:deploy_to]}/current/.htaccess" do
     source ".htaccess.erb"
     mode 0660
