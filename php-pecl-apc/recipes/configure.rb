dbecdb170be7d053e8e169283d375927c0772a30
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-28 13:05:50 -0400

    Fix permissions, enable extension in ini file

--- php-pecl-apc/recipes/configure.rb
+++ php-pecl-apc/recipes/configure.rb
@@ -2,20 +2,16 @@ etc_dir = "/etc/php.d/"
 conf_apc    = "apc.ini"
 
 template "#{etc_dir}/#{conf_apc}" do
+  
+  owner 'root'
+  group 'root'
   mode     "0755"
   source   "apc.ini.erb"
+  
   variables(
     :enabled        => (node["php-pecl-apc"]["enabled"]  rescue '1'),
     :shm_size       => (node["php-pecl-apc"]["shm_size"] rescue '128M'),
     :gc_ttl         => (node["php-pecl-apc"]["gc_ttl"]   rescue '900')
   )
   
-  if platform?("ubuntu")
-    owner "www-data"
-    group "www-data"
-  elsif platform?("amazon")   
-    owner "apache"
-    group "apache"
-  end
-
 end
