d6e5b5600856df9431edc862527ce8b13a0ad016
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-31 19:57:41 -0400

    Update default.rb to include GeoIP attributes for cookbook

--- php-pecl-apc/attributes/default.rb
+++ php-pecl-apc/attributes/default.rb
@@ -2,3 +2,8 @@ default["php-pecl-apc"] = {}
 default["php-pecl-apc"]["enabled"] = '1'
 default["php-pecl-apc"]["shm_size"] = '256M'
 default["php-pecl-apc"]["gc_ttl"] = '600'
+
+default["php-pecl-apc"]['geoip'] = {}
+default["php-pecl-apc"]['geoip']['path'] = "/usr/share/GeoIP/"
+default["php-pecl-apc"]['geoip']['country_dat_url'] = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz"
+default["php-pecl-apc"]['geoip']['city_dat_url']    = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
