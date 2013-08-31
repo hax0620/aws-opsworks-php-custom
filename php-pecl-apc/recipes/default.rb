e523d29f77e893a8988ad26ffa8acea536041ed2
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-31 19:48:51 -0400

    Include system packages to be installed

--- php-pecl-apc/recipes/default.rb
+++ php-pecl-apc/recipes/default.rb
@@ -1,4 +1,21 @@
+include_recipe "apache2"
 include_recipe "apache2::service"
+
+packages = []
+
+# install necessary packages: memcached, apc, geoip-devel
+packages = [
+  'php-pecl-memcached',
+  'php-pecl-apc',
+  'geoip-devel'
+]
+
+packages.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
 include_recipe "php-pecl-apc::configure"
 
 service "apache2" do
