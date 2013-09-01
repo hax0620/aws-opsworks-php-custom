c0d0d8989216fb8c472239e17707efea10f55730
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-31 20:20:57 -0400

    Update default.rb

--- php-pecl-apc/recipes/default.rb
+++ php-pecl-apc/recipes/default.rb
@@ -5,14 +5,23 @@ packages = []
 # install necessary packages: memcached, apc, geoip-devel
 packages = [
   'php-pecl-memcached',
-  'php-pecl-apc',
-  'geoip-devel'
+  'php-pecl-apc'
 ]
 
 packages.each do |pkg|
   package pkg do
     action :install
   end
+end
+
+execute "install geoip-devel" do
+  command "yum install geoip-devel -y"
+  action :run
+end
+
+execute "install pecl geoip" do
+  command "sudo pecl install geoip"
+  action :run
 end
 
 include_recipe "php-pecl-apc::configure"
