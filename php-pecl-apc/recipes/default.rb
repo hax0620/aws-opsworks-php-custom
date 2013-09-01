7f0278648e05e8793e615d791131b09db5781bea
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-31 21:12:21 -0400

    Download and install GeoIP libraries (if needed)

--- php-pecl-apc/recipes/default.rb
+++ php-pecl-apc/recipes/default.rb
@@ -35,6 +35,36 @@ bash "enable_geoip_module_for_php" do
   EOH
 end
 
+bash "download_geoip_db" do
+  not_if do
+    File.exists?("/usr/share/GeoIP/GeoIP.dat") &&
+    File.mtime("/usr/share/GeoIP/GeoIP.dat") > Time.now - 86400
+  end
+  user "root"
+  code <<-EOH
+    cd /tmp
+    rm -f /tmp/GeoIP.dat.gz
+    wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+    gunzip GeoIP.dat.gz
+    sudo mv -f GeoIP.dat /usr/share/GeoIP/GeoIP.dat
+  EOH
+end
+
+bash "download_geolite_city_db" do
+  not_if do
+    File.exists?("/usr/share/GeoIP/GeoIPCity.dat") &&
+    File.mtime("/usr/share/GeoIP/GeoIPCity.dat") > Time.now - 86400
+  end
+  user "root"
+  code <<-EOH
+    cd /tmp
+    rm -f /tmp/GeoLiteCity.dat.gz
+    wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+    gunzip GeoLiteCity.dat.gz
+    sudo mv -f GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
+  EOH
+end
+
 include_recipe "php-pecl-apc::configure"
 
 service "apache2" do
